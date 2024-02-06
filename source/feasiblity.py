442593d3c453f43140afd816c0abad0a878a678e
0Jmins0 <3234252073@qq.com>
2024-02-07 01:27:36 +0800

    调DP
    理论上调完了DP，包括带固定Fi的和不固定时间的

--- source/feasiblity.py
+++ source/feasiblity.py
@@ -85,8 +85,8 @@ def check_with_timewindow(route_pool,instance,Dis_List,Time_Window):
 
         dp[0][Battery_Capacity][Battery_Capacity][0] = 0
         dp[0][Battery_Capacity][Battery_Capacity][1] = 0
-        dp[0][Battery_Capacity][Battery_Capacity][2] = 0 # 最早到达时间
-        dp[0][Battery_Capacity][Battery_Capacity][3] = 0 # 最晚离开时间
+        dp[0][Battery_Capacity][Battery_Capacity][2] = 0 # 到达时间
+        dp[0][Battery_Capacity][Battery_Capacity][3] = 0 # 离开时间
 
         for i in range(1,N):
 
@@ -94,23 +94,24 @@ def check_with_timewindow(route_pool,instance,Dis_List,Time_Window):
             charge = dis * P_Dis_Charge
             time = dis * P_Delivery_Speed
 
-            time_L = Time_Window[index][i][0] + instance['s'][route[i]] # 最早离开
-            time_R = Time_Window[index][i][1] + instance['s'][route[i]] # 最晚离开时间
+            time_L = Time_Window[index][i][0] + instance['s'][route[i]] # 能够，最早离开旳时间
+            time_R = Time_Window[index][i][1] + instance['s'][route[i]] # 必须，最晚离开的时间
 
             # 不充电
             for j in range(0,Battery_Capacity + 1):
                 if(j + charge <= Battery_Capacity):
-                    dp[i][j][0][0] = dp[i - 1][j + charge][0][0]
-                    dp[i][j][0][1] = dp[i - 1][j + charge][0][1]
-                    for k in range(1,Battery_Capacity + 1):
+                    # dp[i][j][0][0] = dp[i - 1][j + charge][0][0]
+                    # dp[i][j][0][1] = dp[i - 1][j + charge][0][1]
+                    for k in range(0,Battery_Capacity + 1):
                         if(k <= j + charge):
                             if(dp[i][j][0][0] > dp[i - 1][j + charge][k][0] and dp[i - 1][j + charge][k][3] + time + instance['s'][route[i]] < time_R):
                                 dp[i][j][0][0] = dp[i - 1][j + charge][k][0]
                                 dp[i][j][0][1] = dp[i - 1][j + charge][k][1]
-                                dp[i][j][0][2] = dp[i - 1][j + charge][k][3] + time # 最早到达
-                                dp[i][j][0][3] = max(dp[i][j][0][2],instance['tl'][route[i]]) + instance['s'][route[i]] # 最早离开
+                                dp[i][j][0][2] = dp[i - 1][j + charge][k][3] + time # 到达时间
+                                dp[i][j][0][3] = max(dp[i][j][0][2],instance['tl'][route[i]]) + instance['s'][route[i]] # 离开时间
                                 Pre_Node[i][j][0] = copy.deepcopy(Pre_Node[i - 1][j + charge][k])
 
+                # print('i:',i," j:",j," prenode:",Pre_Node[i][j][0])
                     # early_charge_finish = dp[i - 1][j + charge][2] + dp[i - 1][j + charge][4]
                     # early_serve_finish = max(dp[i - 1][j + charge][2],instance['tl'][route[i]]) + instance['s'][route[i]]
 
@@ -123,16 +124,18 @@ def check_with_timewindow(route_pool,instance,Dis_List,Time_Window):
                         continue
                     dp[i][j][k][0] = dp[i][j - k][0][0] + Dis_List[route[pre]][route[i]][2]
                     dp[i][j][k][1] = i
-                    Pre_Node[i][j][k] = copy.deepcopy(Pre_Node[i][j - k][0])
-                    Pre_Node[i][j][k].append(i)
                     dp[i][j][k][2] = dp[i][j - k][0][2]
-                    if(dp[i][j][k][2] < instance['tl'][route[i]]): # 如果最早到达时间小于最早服务时间，可以先开始充电
+                    if(dp[i][j][k][2] < instance['tl'][route[i]]): # 如果到达时间小于最早服务时间，可以先开始充电，可边服务边充电
                         dp[i][j][k][3] = max(dp[i][j][k][2] + charge_time,instance['tl'][route[i]] + instance['s'][route[i]])
                     else:
                         dp[i][j][k][3] = dp[i][j][k][2] + max(charge_time,instance['s'][route[i]])
                     if(dp[i][j][k][3] > time_R):
                         dp[i][j][k][0] = float('inf')
                         dp[i][j][k][1] = -1
+                        continue
+                    Pre_Node[i][j][k] = copy.deepcopy(Pre_Node[i][j - k][0])
+                    Pre_Node[i][j][k].append(i)
+                    # print('i:',i," j:",j,' k:',k, " pre:", pre, "prenode:",Pre_Node[i][j][k],"pre list",Pre_Node[i][j - k][0])
 
         Min_dis = float('inf')
         Min_disj = -1
@@ -144,11 +147,12 @@ def check_with_timewindow(route_pool,instance,Dis_List,Time_Window):
                     Min_dis = dp[N - 1][j][k][0]
                     Min_disj = j
                     Min_disk = k
-        print("Min_dis",Min_dis)
-        print("Min_disj",Min_disj)
-        print("Min_disk",Min_disk)
-        print(Pre_Node[N - 1][Min_disj][Min_disk])
+        # print("Min_dis",Min_dis)
+        # print("Min_disj",Min_disj)
+        # print("Min_disk",Min_disk)
+        # print(Pre_Node[N - 1][Min_disj][Min_disk])
 
+        # print(Pre_Node)
 
         # for i in range(N):
         #     print(i,":",Pre_Node[i])
@@ -167,8 +171,8 @@ def check_with_regular_time(route_pool,instance,Dis_List,Time_Window): #DP部分
     for route in route_pool:
         N = len(route)
 
-        begin = 0
-        end = 0
+        begin = 0 # 到达时间
+        end = 0 # 离开时间
         f = np.zeros(N).tolist()
 
         # 预处理充电量
@@ -178,8 +182,12 @@ def check_with_regular_time(route_pool,instance,Dis_List,Time_Window): #DP部分
             f[i] = (end - begin) * P_Charge_Time
 
         # print('f',f)
-
+        # dp[i][j][0/1][0/1]
+        # 离开第 i 个点，有 j 的电量时，（不充/充电）时的（消费/上一个充电的点）
         dp = np.zeros((N + 2, Battery_Capacity + 1,2, 2)).tolist()
+
+        # Pre_Node[i][j][0/1]
+        # 离开第 i 个点，有 j 的电量时, （不充/充电）的充电路径列表
         Pre_Node = [[[[] for _ in range(2)] for _ in range(Battery_Capacity + 1)] for _ in range(N + 1)]
         # Pre_Node[0].append(0)
 
@@ -197,10 +205,13 @@ def check_with_regular_time(route_pool,instance,Dis_List,Time_Window): #DP部分
 
 
         for i in range(1, N):
+
+            # i - 1 到 i 的耗电量
             dis = Dis_List[route[i]][route[i - 1]][2]
             charge = dis * P_Dis_Charge
 
             # 不充电
+            # dp[i][j][0][0] <--- dp[i - 1][j + charge][0/1][0]
             for j in range(0, Battery_Capacity):
                 if (j + charge <= Battery_Capacity):
                     if(dp[i - 1][j + charge][0][0] < dp[i][j + charge][1][0]):
@@ -246,10 +257,10 @@ def check_with_regular_time(route_pool,instance,Dis_List,Time_Window): #DP部分
                     Min_disj = j
                     Min_disk = k
 
-        print("Min_dis",Min_dis)
-        print("Min_disj",Min_disj)
-        print("Min_disk",Min_disk)
-        print(Pre_Node[N - 1][Min_disj][Min_disk])
+        # print("Min_dis",Min_dis)
+        # print("Min_disj",Min_disj)
+        # print("Min_disk",Min_disk)
+        # print(Pre_Node[N - 1][Min_disj][Min_disk])
 
 
         # for i in range(N):
